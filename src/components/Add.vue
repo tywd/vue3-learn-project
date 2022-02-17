657785ecf81ad0b99bc06788636f9f3a2fa8b5f0
shichuyu <1042048096@qq.com>
2022-02-17 13:09:05 +0800

    composition + script setup

---
+++ src/components/Add.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div>
+    <h1 ><button @click="add">+</button>{{ count }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let count = ref(1)
+let color = ref('red')
+function add() {
+  count.value++
+  color.value = Math.random()>0.5? "blue":"red"
+}
+</script>
+
+<style scoped>
+h1 {
+  color:v-bind(color); /* 直接使用js中的变量 */
+}
+</style>>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,11 @@
   <div>
     <h1>这是首页</h1>
     <TodoList />
+    <Add />
   </div>
 </template>
 
 <script setup>
 import TodoList from "../components/Todolist.vue";
+import Add from "../components/Add.vue";
 </script>
