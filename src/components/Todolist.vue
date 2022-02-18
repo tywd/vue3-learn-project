ca5908e41b34eb439abc5fce096e7e39f73f397a
shichuyu <1042048096@qq.com>
2022-02-18 12:05:43 +0800

    基础入门篇结束

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,24 +1,42 @@
 <!-- v3 写法 -->
 <template>
   <div>
+    <span class="dustbin"> 🗑 </span>
     <input type="text" v-model="title" @keydown.enter="addTodo" />
     <button v-if="active < all" @click="clear">清理</button>
     <ul v-if="todos.length">
-      <li v-for="(todo, index) in todos" :key="index">
-        <input type="checkbox" v-model="todo.done" />
-        <span :class="{ done: todo.done }"> {{ todo.title }}</span>
-      </li>
+      <transition-group name="flip-list" tag="ul">
+        <li v-for="(todo, index) in todos" :key="index">
+          <input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }"> {{ todo.title }}</span>
+          <span class="remove-btn" @click="removeTodo($event, i)"> ❌ </span>
+        </li>
+      </transition-group>
     </ul>
     <div v-else>暂无数据</div>
     <div>
       全选<input type="checkbox" v-model="allDone" />
       <span> {{ active }} / {{ all }} </span>
     </div>
+    <transition name="modal">
+      <div class="info-wrapper" v-if="showModal">
+        <div class="info">哥，你啥也没输入！</div>
+      </div>
+    </transition>
+    <div class="animate-wrap">
+      <transition
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+      >
+        <div class="animate" v-show="animate.show">📋</div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 import { useMouse } from "../utils/mouse";
 let { x, y } = useMouse();
 /* let title = ref("");
@@ -50,49 +68,105 @@ function add() {
   count.value++;
 }
 
-let { title, todos, addTodo, clear, active, all, allDone } = useTodos();
+let {
+  title,
+  todos,
+  showModal,
+  addTodo,
+  removeTodo,
+  clear,
+  active,
+  all,
+  allDone,
+  animate,
+  beforeEnter,
+  enter,
+  afterEnter,
+} = useTodos();
 
 function useTodos() {
-    let title = ref("");
-    let todos = ref([{
-        title: "学习Vue",
-        done: false
-    }]);
+  let title = ref("");
+  let todos = ref([
+    {
+      title: "学习Vue",
+      done: false,
+    },
+  ]);
+  let showModal = ref(false);
 
-    function addTodo() {
-        todos.value.push({
-            title: title.value,
-            done: false,
-        });
-        title.value = "";
+  function addTodo() {
+    if (!title.value) {
+      showModal.value = true;
+      setTimeout(() => {
+        showModal.value = false;
+      }, 1500);
+      return;
     }
+    todos.value.push({
+      title: title.value,
+      done: false,
+    });
+    title.value = "";
+  }
 
-    function clear() {
-        todos.value = todos.value.filter((v) => !v.done);
-    }
-    let active = computed(() => {
-        return todos.value.filter((v) => !v.done).length;
-    });
-    let all = computed(() => todos.value.length);
-    let allDone = computed({
-        get: function () {
-            return active.value === 0;
-        },
-        set: function (value) {
-            todos.value.forEach((todo) => {
-                todo.done = value;
-            });
-        },
-    });
-    return {
-        title,
-        todos,
-        addTodo,
-        clear,
-        active,
-        all,
-        allDone
-    };
+  function removeTodo(e, i) {
+    todos.value.splice(i, 1);
+  }
+
+  function clear() {
+    todos.value = todos.value.filter((v) => !v.done);
+  }
+  let active = computed(() => {
+    return todos.value.filter((v) => !v.done).length;
+  });
+  let all = computed(() => todos.value.length);
+  let allDone = computed({
+    get: function () {
+      return active.value === 0;
+    },
+    set: function (value) {
+      todos.value.forEach((todo) => {
+        todo.done = value;
+      });
+    },
+  });
+  let animate = reactive({ show: false, el: null });
+  function beforeEnter(el) {
+    let dom = animate.el;
+    let rect = dom.getBoundingClientRect();
+    let x = window.innerWidth - rect.left - 60;
+    let y = rect.top - 10;
+    el.style.transform = `translate(-${x}px, ${y}px)`;
+  }
+  function enter(el, done) {
+    document.body.offsetHeight;
+    el.style.transform = `translate(0,0)`;
+    el.addEventListener("transitionend", done);
+  }
+  function afterEnter(el) {
+    animate.show = false;
+    el.style.display = "none";
+  }
+  function removeTodo(e, i) {
+    animate.el = e.target;
+    animate.show = true;
+    todos.value.splice(i, 1);
+  }
+  return {
+    title,
+    todos,
+    showModal,
+    addTodo,
+    removeTodo,
+    clear,
+    active,
+    all,
+    allDone,
+    animate,
+    beforeEnter,
+    enter,
+    afterEnter,
+  };
 }
 </script>
 
@@ -100,4 +174,55 @@ function useTodos() {
 h1 {
   color: red;
 }
-</style>>
+.info-wrapper {
+  position: fixed;
+  top: 20px;
+  width: 200px;
+}
+.info {
+  padding: 20px;
+  color: white;
+  background: #d88986;
+}
+.modal-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.modal-enter-active {
+  transition: all 0.3s ease;
+}
+.modal-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.modal-leave-active {
+  transition: all 0.3s ease;
+}
+.flip-list-move {
+  transition: transform 0.8s ease;
+}
+.flip-list-enter-active,
+.flip-list-leave-active {
+  transition: all 1s ease;
+}
+.flip-list-enter-from,
+.flip-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.remove-btn {
+  cursor: pointer;
+}
+.animate-wrap .animate {
+  position: fixed;
+  right: 10px;
+  top: 10px;
+  z-index: 100;
+  transition: all 0.5s linear;
+}
+.dustbin{
+  position:fixed;
+  top: 10px;
+  right: 10px;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>这是首页</h1>
-    <!-- <TodoList /> -->
-    <TodoListV2 />
+    <TodoList />
+    <!-- <TodoListV2 /> -->
     <Add />
   </div>
 </template>
 
 <script setup>
-// import TodoList from "../components/TodoList.vue";
-import TodoListV2 from "../components/TodolistV2.vue";
+import TodoList from "../components/TodoList.vue";
+// import TodoListV2 from "../components/TodolistV2.vue";
 import Add from "../components/Add.vue";
 </script>
