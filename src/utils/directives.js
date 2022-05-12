5f5eb67f910d960825a2f252e0ed8112b0b58c77
shichuyu <1042048096@qq.com>
2022-05-12 14:02:32 +0800

    update: vue指令说明

--- src/utils/directives.js
+++ src/utils/directives.js
@@ -1,7 +1,27 @@
 // 自定义指令
+// https://v3.cn.vuejs.org/api/application-api.html#directive
+// https://v3.cn.vuejs.org/guide/migration/custom-directives.html
+/* Vue2.x 
+bind - 指令绑定到元素后调用。只调用一次。
+inserted - 元素插入父 DOM 后调用。
+update - 当元素更新，但子元素尚未更新时，将调用此钩子。
+componentUpdated - 一旦组件和子级被更新，就会调用这个钩子。
+unbind - 一旦指令被移除，就会调用这个钩子。也只调用一次。 */
+
+/* Vue3.x
+created - 新增！在元素的 attribute 或事件监听器被应用之前调用。
+bind → beforeMount
+inserted → mounted
+beforeUpdate：新增！在元素本身被更新之前调用，与组件的生命周期钩子十分相似。
+update → 移除！该钩子与 updated 有太多相似之处，因此它是多余的。请改用 updated。
+componentUpdated → updated
+beforeUnmount：新增！与组件的生命周期钩子类似，它将在元素被卸载之前调用。
+unbind -> unmounted
+ */
 import {
     message,
 } from 'ant-design-vue'
+import { Vue2 } from 'vue-global-api/node_modules/vue-demi';
 const demo = {
     // Vue3.x 的生命周期
     // 指令具有一组生命周期钩子：
